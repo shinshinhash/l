745b8a0ea8fc41bf164cb84c9a3b6600cb8f5408
Shintaro Hashimoto <shinshinhash@gmail.com>
2014-04-28 00:40:22 +0400

    Followed the tutorial to deploy instagram

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,9 @@
+require "instagram"
+
+Instagram.configure do |config|
+    
+    config.client_id = "66bcfd92b5a1443a8d0dac820225eba3"
+    
+    config.access_token = "5f8f72267cdb496c9bf9742edaf56da3"
+    
+end
